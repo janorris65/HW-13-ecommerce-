134ad765306ab2502882391b598259af47ad0941
Joshua <janorris65@gmail.com>
2023-01-22 21:49:14 -0600

    set up first associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Category.hasMany(Product);
 // Categories have many Products
-
+Product.belongsTo(Category);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
