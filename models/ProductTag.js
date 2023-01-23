78c57a2451d978db407111f010465b02ca53733d
Joshua <janorris65@gmail.com>
2023-01-22 20:41:59 -0600

    first models made

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,11 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      autoIncrement:true,
+      primaryKey:true
+    },
+    product_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:"product",
+        key:"id"
+      }
+    },
+    tag_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:"tag",
+        key:"id"
+      }
+    }
     // define columns
   },
   {
